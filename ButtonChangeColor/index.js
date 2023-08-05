8f8d7ac1319e3de035327bfc6dd3e03b4b2ca903
Utsha Kumar Das <75168319+UtshadasCSE@users.noreply.github.com>
2023-08-05 23:10:00 +0600

    Add files via upload

---
+++ ButtonChangeColor/index.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  btn.style.background = "#9BE8D8";
+  btn.style.boxShadow = "0 0 40 #9BE8D8";
+  btn.style.transition = ".5 ease";
+});
